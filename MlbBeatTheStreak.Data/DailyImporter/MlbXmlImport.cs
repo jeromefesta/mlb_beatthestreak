b3aa0f4783af83ef1a819e987547d6444ca5958a
Jerome Festa <jerome.festa@gmail.com>
2017-07-20 15:04:06 -0400

    Started writing method stubs today for the import process.

--- MlbBeatTheStreak.Data/DailyImporter/MlbXmlImport.cs
+++ MlbBeatTheStreak.Data/DailyImporter/MlbXmlImport.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using MlbBeatTheStreak.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace MlbBeatTheStreak.Data
 {
     public class MlbXmlImport
     {
+        public GameModel GetTorYankGame() {
+            XDocument mlbGameDetailsDocument = LoadXml();
+            GameModel game = GetGame(mlbGameDetailsDocument);
+            return game;
+        }
 
+        private GameModel GetGame(XDocument mlbGameDetailsDocument)
+        {
+            throw new NotImplementedException();
+        }
+
+        private XDocument LoadXml()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
